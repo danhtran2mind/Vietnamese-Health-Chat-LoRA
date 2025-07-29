3a113b5109a778628e84358e0a55d4733d08ff62
Danh Tran <cprmmb99@gmail.com>
2025-07-29 18:15:38 +0700

    Delete apps/gradio_app directory

--- apps/gradio_app/utils.py
+++
@@ -1,5 +0,0 @@
-def merge_conversation(conversation):
-    valid_pairs = [(q, a) for q, a in conversation if a is not None]
-    formatted_pairs = [f"{q} {a}." for q, a in valid_pairs]
-    result = ["-".join(formatted_pairs) + "\n"]
-    return result
